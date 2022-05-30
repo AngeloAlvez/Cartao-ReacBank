468d767358b41eba5bb9c3db5d00994e3cdad967
Angelo Alves <angelo.alves@grupoboticario.com.br>
2022-05-30 17:51:33 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import { Cartao } from './components/Cartao';
+import styled from 'styled-components'
 
+let cor ='#FF7900'
+
+const DivMain = styled.div`
+display: flex;
+flex-direction: column;
+align-items:center;
+color: black;
+`
+const InputMain = styled.input`
+text-align: center;
+width: 300px;
+border: 3px solid ${cor}
+border-radius: 4px;
+font-size:20px;
+padding: 10px;
+margin-bottom: 30px;
+`
 function App() {
+
+  const [inputName, setInputName] = useState('NOME COMPLETO')
+
+  const [inputNumero, setInputNumero] = useState('0000  0000  0000  0000')
+
+
+  const handleInputName = (event) => {
+    setInputName(event.target.value)
+  }
+  const handleInputNumero = (event) => {
+    setInputNumero(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DivMain>
+      <h1>Informe seus dados e veja seu cartão:</h1>
+      <InputMain value={inputName} onChange={handleInputName}></InputMain>
+      <InputMain value={inputNumero} onChange={handleInputNumero}></InputMain>
+      <Cartao numero={inputNumero} nome={inputName}>
+      </Cartao>
+    </DivMain>
   );
 }
 
---
+++ src/components/Cartao.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from 'styled-components'
+import { useState } from 'react';
+
+const MainDiv = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+
+`
+const CardDiv = styled.div`
+width: 450px;
+height: 250px;
+background-color: ${({toggle}) => toggle ? '#03BEEC': '#2b2929'};
+border-radius: 20px;
+display: flex;
+flex-direction: column;
+align-items:center;
+justify-content: space-around;
+box-shadow: 5px 5px 10px #9b9999;
+`
+
+/////
+const LogoReact = styled.div`
+display:flex;
+align-items:center;
+`
+const CardLogo = styled.div`
+color: white;
+font-family: arial;
+font-size:40px;
+font-weight: bolder;
+`
+const SimboloReact = styled.div`
+color:white;
+font-size:70px;
+font-weight: bolder;
+padding: 5px;
+`
+const CardName = styled.h2`
+font-family: monospace;
+color: white;
+text-shadow: 2px 2px rgba(94, 92, 92, 0.628);
+font-size: 25px;
+letter-spacing: 4px;
+`
+const CardDivMaster = styled.div`
+width:450px;
+display:flex;
+flex-direction:row;
+align-items:center;
+justify-content: space-around;
+`
+const CardMasterLogo = styled.img`
+width: 60px;
+`
+const CardBlackButton = styled.button`
+border: 2px solid black;
+border-radius: 10px;
+font-size: 25px;
+padding: 10px;
+background-color: white;
+margin-bottom: 20px;
+`
+
+
+export function Cartao(props){
+
+    const [toggle, setToggle] = React.useState(true);
+
+    return(
+        <MainDiv>
+            <CardBlackButton onClick={e => setToggle(state => !state)}> 🔄 Cliente Standart / Black</CardBlackButton> 
+
+            <CardDiv toggle={toggle}>
+                <LogoReact>
+                    <CardLogo>React Bank</CardLogo>
+                    <SimboloReact>&#9883;</SimboloReact>
+                </LogoReact>
+                <CardName>{props.numero}</CardName>
+
+                <CardDivMaster>
+                <CardName>{props.nome}</CardName>
+                <CardMasterLogo src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Mastercard_2019_logo.svg/1280px-Mastercard_2019_logo.svg.png" />
+                </CardDivMaster>
+            </CardDiv>
+        </MainDiv>
+    )
+}
